bc2676f26f63acfb4f783655e801ea5069f23d42
Getulio Valentin Sánchez <gvso@google.com>
2020-07-23 17:57:36 -0400

    stackdriver: Use package with RPC for MQL query (#19)

--- internal/stackdriver/stackdriver.go
+++ internal/stackdriver/stackdriver.go
@@ -5,19 +5,18 @@ import (
 	"fmt"
 	"time"
 
-	monitoring "cloud.google.com/go/monitoring/apiv3"
 	"github.com/GoogleCloudPlatform/cloud-run-release-operator/internal/metrics"
 	"github.com/pkg/errors"
-	"google.golang.org/api/iterator"
-	monitoringpb "google.golang.org/genproto/googleapis/monitoring/v3"
-	duration "google.golang.org/protobuf/types/known/durationpb"
-	timestamp "google.golang.org/protobuf/types/known/timestamppb"
+
+	// TODO: Migrate to cloud.google.com/go/monitoring/apiv3/v2 once RPC for MQL
+	// query is added (https://cloud.google.com/monitoring/api/ref_v3/rest/v3/projects.timeSeries/query).
+	monitoring "google.golang.org/api/monitoring/v3"
 )
 
 // API is a wrapper for the Cloud Monitoring package.
 type API struct {
-	*monitoring.MetricClient
-	Project string
+	MetricClient *monitoring.Service
+	Project      string
 }
 
 // Query is the filter used to retrieve metrics data.
@@ -33,7 +32,7 @@ const (
 
 // NewAPIClient initializes
 func NewAPIClient(ctx context.Context, project string) (*API, error) {
-	client, err := monitoring.NewMetricClient(ctx)
+	client, err := monitoring.NewService(ctx)
 	if err != nil {
 		return nil, errors.Wrap(err, "could not initialize Cloud Metics client")
 	}
@@ -48,68 +47,67 @@ func NewAPIClient(ctx context.Context, project string) (*API, error) {
 func (a *API) Latency(ctx context.Context, query metrics.Query, offset time.Duration, alignReduceType metrics.AlignReduce) (float64, error) {
 	query = addFilterToQuery(query, "metric.type", requestLatencies)
 	endTime := time.Now()
-	startTime := time.Now().Add(-1 * offset)
+	startTime := endTime.Add(-1 * offset)
 	aligner, reducer := alignerAndReducer(alignReduceType)
+	offsetString := fmt.Sprintf("%fs", offset.Seconds())
 
-	it := a.MetricClient.ListTimeSeries(ctx, &monitoringpb.ListTimeSeriesRequest{
-		Name:   "projects/" + a.Project,
-		Filter: query.Query(),
-		Interval: &monitoringpb.TimeInterval{
-			StartTime: timestamp.New(startTime),
-			EndTime:   timestamp.New(endTime),
-		},
-		Aggregation: &monitoringpb.Aggregation{
-			AlignmentPeriod:    duration.New(offset),
-			PerSeriesAligner:   aligner,
-			GroupByFields:      []string{"metric.labels.response_code_class"},
-			CrossSeriesReducer: reducer,
-		},
-	})
+	req := a.MetricClient.Projects.TimeSeries.List("projects/" + a.Project).
+		Filter(query.Query()).
+		IntervalStartTime(startTime.Format(time.RFC3339Nano)).
+		IntervalEndTime(endTime.Format(time.RFC3339Nano)).
+		AggregationAlignmentPeriod(offsetString).
+		AggregationPerSeriesAligner(aligner).
+		AggregationGroupByFields("metric.labels.response_code_class").
+		AggregationCrossSeriesReducer(reducer)
 
-	return latencyForCodeClass(it, "2xx")
+	resp, err := req.Do()
+	if err != nil {
+		return 0, errors.Wrap(err, "error when retrieving time series")
+	}
+	if len(resp.ExecutionErrors) != 0 {
+		return 0, errors.Errorf("execution errors occurred: %v", resp.ExecutionErrors)
+	}
+
+	return latencyForCodeClass(resp.TimeSeries, "2xx")
 }
 
 // ErrorRate returns the rate of 5xx errors for the resource matching the filter.
 func (a *API) ErrorRate(ctx context.Context, query metrics.Query, offset time.Duration) (float64, error) {
 	query = addFilterToQuery(query, "metric.type", requestCount)
 	endTime := time.Now()
-	startTime := time.Now().Add(-1 * offset)
+	startTime := endTime.Add(-1 * offset)
+	offsetString := fmt.Sprintf("%fs", offset.Seconds())
 
-	it := a.MetricClient.ListTimeSeries(ctx, &monitoringpb.ListTimeSeriesRequest{
-		Name:   "projects/" + a.Project,
-		Filter: query.Query(),
-		Interval: &monitoringpb.TimeInterval{
-			StartTime: timestamp.New(startTime),
-			EndTime:   timestamp.New(endTime),
-		},
-		Aggregation: &monitoringpb.Aggregation{
-			AlignmentPeriod:    duration.New(offset),
-			PerSeriesAligner:   monitoringpb.Aggregation_ALIGN_DELTA,
-			GroupByFields:      []string{"metric.labels.response_code_class"},
-			CrossSeriesReducer: monitoringpb.Aggregation_REDUCE_SUM,
-		},
-	})
+	req := a.MetricClient.Projects.TimeSeries.List("projects/" + a.Project).
+		Filter(query.Query()).
+		IntervalStartTime(startTime.Format(time.RFC3339Nano)).
+		IntervalEndTime(endTime.Format(time.RFC3339Nano)).
+		AggregationAlignmentPeriod(offsetString).
+		AggregationPerSeriesAligner("ALIGN_DELTA").
+		AggregationGroupByFields("metric.labels.response_code_class").
+		AggregationCrossSeriesReducer("REDUCE_SUM")
 
-	return calculateErrorResponseRate(it)
+	resp, err := req.Do()
+	if err != nil {
+		return 0, errors.Wrap(err, "error when retrieving time series")
+	}
+	if len(resp.ExecutionErrors) != 0 {
+		return 0, errors.Errorf("execution errors occurred: %v", resp.ExecutionErrors)
+	}
+
+	return calculateErrorResponseRate(resp.TimeSeries)
 }
 
 // latencyForCodeClass retrieves the latency for a given response code class
 // (e.g. 2xx, 5xx, etc.)
-func latencyForCodeClass(it *monitoring.TimeSeriesIterator, codeClass string) (float64, error) {
+func latencyForCodeClass(timeSeries []*monitoring.TimeSeries, codeClass string) (float64, error) {
 	var latency float64
-	for {
-		series, err := it.Next()
-		if err == iterator.Done {
-			break
-		}
-		if err != nil {
-			return 0, errors.Wrap(err, "error when iterating through time series")
-		}
-
+	for _, series := range timeSeries {
 		// Because the interval and the series aligner are the same, only one
 		// point is returned per time series.
 		if series.Metric.Labels["response_code_class"] == codeClass {
-			latency = series.Points[0].Value.GetDoubleValue()
+			latency = *(series.Points[0].Value.DoubleValue)
+			break
 		}
 	}
 
@@ -122,25 +120,17 @@ func latencyForCodeClass(it *monitoring.TimeSeriesIterator, codeClass string) (f
 // add them up to form a 'total'. Then, it divides the number of error responses
 // by the total.
 // It ignores any other type of responses (e.g. 4xx).
-func calculateErrorResponseRate(it *monitoring.TimeSeriesIterator) (float64, error) {
+func calculateErrorResponseRate(timeSeries []*monitoring.TimeSeries) (float64, error) {
 	var errorResponseCount, successfulResponseCount int64
-	for {
-		series, err := it.Next()
-		if err == iterator.Done {
-			break
-		}
-		if err != nil {
-			return 0, errors.Wrap(err, "error when iterating through time series")
-		}
-
+	for _, series := range timeSeries {
 		// Because the interval and the series aligner are the same, only one
 		// point is returned per time series.
 		switch series.Metric.Labels["response_code_class"] {
 		case "5xx":
-			errorResponseCount += series.Points[0].Value.GetInt64Value()
+			errorResponseCount += *(series.Points[0].Value.Int64Value)
 			break
 		default:
-			successfulResponseCount += series.Points[0].Value.GetInt64Value()
+			successfulResponseCount += *(series.Points[0].Value.Int64Value)
 			break
 		}
 	}
@@ -155,19 +145,19 @@ func calculateErrorResponseRate(it *monitoring.TimeSeriesIterator) (float64, err
 	return rate, nil
 }
 
-func alignerAndReducer(alignReduceType metrics.AlignReduce) (aligner monitoringpb.Aggregation_Aligner, reducer monitoringpb.Aggregation_Reducer) {
+func alignerAndReducer(alignReduceType metrics.AlignReduce) (aligner string, reducer string) {
 	switch alignReduceType {
 	case metrics.Align99Reduce99:
-		aligner = monitoringpb.Aggregation_ALIGN_PERCENTILE_99
-		reducer = monitoringpb.Aggregation_REDUCE_PERCENTILE_99
+		aligner = "ALIGN_PERCENTILE_99"
+		reducer = "REDUCE_PERCENTILE_99"
 		break
 	case metrics.Align95Reduce95:
-		aligner = monitoringpb.Aggregation_ALIGN_PERCENTILE_95
-		reducer = monitoringpb.Aggregation_REDUCE_PERCENTILE_95
+		aligner = "ALIGN_PERCENTILE_95"
+		reducer = "REDUCE_PERCENTILE_50"
 		break
 	case metrics.Align50Reduce50:
-		aligner = monitoringpb.Aggregation_ALIGN_PERCENTILE_50
-		reducer = monitoringpb.Aggregation_REDUCE_PERCENTILE_50
+		aligner = "ALIGN_PERCENTILE_50"
+		reducer = "REDUCE_PERCENTILE_50"
 		break
 	}
 
